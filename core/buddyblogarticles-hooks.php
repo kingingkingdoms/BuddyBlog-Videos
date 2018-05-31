4247d8194d0cb95f30c277b5280f8f2f074f93b2
kingingkingdoms <37118190+kingingkingdoms@users.noreply.github.com>
2018-05-31 19:42:39 -0400

    Update buddyblogarticles-hooks.php

--- core/buddyblogarticles-hooks.php
+++ core/buddyblogarticles-hooks.php
@@ -12,23 +12,23 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Fix to disable/re-enable buddypress comment open/close filter.
  */
-function buddyblogphotos_disable_bp_comment_filter() {
+function buddyblogarticles_disable_bp_comment_filter() {
 
 	if ( has_filter( 'comments_open', 'bp_comments_open' ) ) {
 		remove_filter( 'comments_open', 'bp_comments_open', 10, 2 );
 	}
 }
 
-add_action( 'buddyblogphotos_before_blog_post', 'buddyblogphotos_disable_bp_comment_filter' );
+add_action( 'buddyblogarticles_before_blog_post', 'buddyblogarticles_disable_bp_comment_filter' );
 
 /**
  * Re enable buddypress comments filter.
  */
-function buddyblogphotos_enable_bp_comment_filter() {
+function buddyblogarticles_enable_bp_comment_filter() {
 
 	if ( function_exists( 'bp_comments_open' ) ) {
 		add_filter( 'comments_open', 'bp_comments_open', 10, 2 );
 	}
 }
 
-add_action( 'buddyblogphotos_after_blog_post', 'buddyblogphotos_enable_bp_comment_filter' );
+add_action( 'buddyblogarticles_after_blog_post', 'buddyblogarticles_enable_bp_comment_filter' );
