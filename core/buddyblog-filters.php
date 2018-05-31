d5fe67f9570b887fa43e538a2211a308699a1c4d
kingingkingdoms <37118190+kingingkingdoms@users.noreply.github.com>
2018-05-31 19:07:09 -0400

    Update buddyblog-filters.php

--- core/buddyblog-filters.php
+++ core/buddyblog-filters.php
@@ -15,15 +15,15 @@
  *
  * @return string
  */
-function buddyblog_fix_comment_form_redirect( $post_id ) {
+function buddyblogphotos_fix_comment_form_redirect( $post_id ) {
 
 	$post = get_post( $post_id );
 
-	if ( ! buddyblog_show_posts_on_profile( $post ) ) {
+	if ( ! buddyblogphotos_show_posts_on_profile( $post ) ) {
 		return $post_id;
 	}
 
-	if ( $post->post_type != buddyblog_get_posttype() ) {
+	if ( $post->post_type != buddyblogphotos_get_posttype() ) {
 		return;
 	}
 
@@ -35,7 +35,7 @@ function buddyblog_fix_comment_form_redirect( $post_id ) {
 	<?php
 }
 
-add_action( 'comment_form', 'buddyblog_fix_comment_form_redirect' );
+add_action( 'comment_form', 'buddyblogphotos_fix_comment_form_redirect' );
 
 /**
  * Get the current editable post id
@@ -46,11 +46,11 @@ add_action( 'comment_form', 'buddyblog_fix_comment_form_redirect' );
  *
  * @return int id of the post to edit
  */
-function buddyblog_get_editable_post_id( $id ) {
+function buddyblogphotos_get_editable_post_id( $id ) {
 
 	$action = bp_current_action();
 
-	if ( bp_is_buddyblog_component() && ( $action == 'edit' ) && bp_action_variable( 0 ) ) {
+	if ( bp_is_buddyblogphotos_component() && ( $action == 'edit' ) && bp_action_variable( 0 ) ) {
 
 		$id = bp_action_variable( 0 );
 	}
@@ -59,7 +59,7 @@ function buddyblog_get_editable_post_id( $id ) {
 	return intval( $id );
 }
 
-add_filter( 'bpsp_editable_post_id', 'buddyblog_get_editable_post_id' );
+add_filter( 'bpsp_editable_post_id', 'buddyblogphotos_get_editable_post_id' );
 
 
 /**
@@ -72,29 +72,29 @@ add_filter( 'bpsp_editable_post_id', 'buddyblog_get_editable_post_id' );
  *
  * @return mixed|string|void
  */
-function buddyblog_filter_posting_message( $message, $post_id, $post_type_obj, $form ) {
+function buddyblogphotos_filter_posting_message( $message, $post_id, $post_type_obj, $form ) {
 
-	if ( $form->get_post_type() != buddyblog_get_posttype() ) {
+	if ( $form->get_post_type() != buddyblogphotos_get_posttype() ) {
 		return $message;
 	}
 
-	if ( ! buddyblog_is_post_published( $post_id ) ) {
+	if ( ! buddyblogphotos_is_post_published( $post_id ) ) {
 		// if the job is not active, let us know that to the user.
-		$message = __( 'Your post was saved.', 'buddyblog' );
+		$message = __( 'Your post was saved.', 'buddyblogphotos' );
 	}
 
 	return $message;
 }
 
-add_filter( 'bsfep_post_success_message', 'buddyblog_filter_posting_message', 10, 4 );
+add_filter( 'bsfep_post_success_message', 'buddyblogphotos_filter_posting_message', 10, 4 );
 
 /**
  * Update the post status based on current user caps
  * If the current user can publish, we will set the status to publish otherwise the status is set to draft by default
  */
 // NOT Needed
-// add_action('bsfep_post_saved','buddyblog_publish_unpublish_on_save');
-function buddyblog_publish_unpublish_on_save( $post_id ) {
+// add_action('bsfep_post_saved','buddyblogphotos_publish_unpublish_on_save');
+function buddyblogphotos_publish_unpublish_on_save( $post_id ) {
 
 	if ( ! empty( $_POST['post_id'] ) ) {
 		return;
@@ -103,12 +103,12 @@ function buddyblog_publish_unpublish_on_save( $post_id ) {
 	// check if it is job post type.
 	$post = get_post( $post_id );
 
-	if ( $post->post_type != buddyblog_get_posttype() ) {
+	if ( $post->post_type != buddyblogphotos_get_posttype() ) {
 		return;
 	}
 
 	// if it is a new post.
-	if ( buddyblog_user_can_publish( get_current_user_id() ) ) {
+	if ( buddyblogphotos_user_can_publish( get_current_user_id() ) ) {
 		//    $active_status=1;
 
 	}
@@ -119,31 +119,31 @@ function buddyblog_publish_unpublish_on_save( $post_id ) {
 /**
  * Fix the post permalink the_permalink to point to user profile/buddyblog/my-posts/ section for custom post type if we are using custom post type
  */
-function buddyblog_fix_permalink( $permalink, $post, $leavename, $sample ) {
+function buddyblogphotos_fix_permalink( $permalink, $post, $leavename, $sample ) {
 
 
-	if ( ! buddyblog_show_posts_on_profile( $post ) ) {
+	if ( ! buddyblogphotos_show_posts_on_profile( $post ) ) {
 		return $permalink;
 	}
 
 
-	$type = buddyblog_get_posttype();
+	$type = buddyblogphotos_get_posttype();
 
 	$user_link = '';
 
 	if ( $post->post_type == $type ) {
 
 		if ( $post->post_status != 'publish' ) {
-			$permalink = buddyblog_get_edit_url( $post->ID );
+			$permalink = buddyblogphotos_get_edit_url( $post->ID );
 		} else {
-			$permalink = buddyblog_get_post_url( $post->ID );
+			$permalink = buddyblogphotos_get_post_url( $post->ID );
 		}
 	}
 
 	return $permalink;
 }
 
-add_filter( 'post_type_link', 'buddyblog_fix_permalink', 10, 4 );
+add_filter( 'post_type_link', 'buddyblogphotos_fix_permalink', 10, 4 );
 
 /**
  * Filter post permalink.
@@ -154,21 +154,21 @@ add_filter( 'post_type_link', 'buddyblog_fix_permalink', 10, 4 );
  *
  * @return string
  */
-function buddyblog_filter_post_permalink( $permalink, $post, $leavename ) {
+function buddyblogphotos_filter_post_permalink( $permalink, $post, $leavename ) {
 
 
-	if ( ! buddyblog_show_posts_on_profile( $post ) ) {
+	if ( ! buddyblogphotos_show_posts_on_profile( $post ) ) {
 		return $permalink;
 	}
 
-	if ( $post->post_type != buddyblog_get_posttype() ) {
+	if ( $post->post_type != buddyblogphotos_get_posttype() ) {
 		return $permalink;
 	}
 
 	if ( $post->post_status != 'publish' ) {
-		$permalink = buddyblog_get_edit_url( $post->ID );
+		$permalink = buddyblogphotos_get_edit_url( $post->ID );
 	} else {
-		$permalink = buddyblog_get_post_url( $post->ID );
+		$permalink = buddyblogphotos_get_post_url( $post->ID );
 	}
 
 	return $permalink;
@@ -176,7 +176,7 @@ function buddyblog_filter_post_permalink( $permalink, $post, $leavename ) {
 
 }
 
-add_filter( 'post_link', 'buddyblog_filter_post_permalink', 10, 3 );
+add_filter( 'post_link', 'buddybloghotos_filter_post_permalink', 10, 3 );
 
 /**
  * Fix edit post link to point to our intended link.
@@ -187,23 +187,23 @@ add_filter( 'post_link', 'buddyblog_filter_post_permalink', 10, 3 );
  *
  * @return string
  */
-function buddyblog_fix_edit_post_link( $edit_url, $post_id, $context ) {
+function buddyblogphotos_fix_edit_post_link( $edit_url, $post_id, $context ) {
 
 	$post = get_post( $post_id );
 
-	if ( $post->post_type != buddyblog_get_posttype() || is_super_admin() ) {
+	if ( $post->post_type != buddyblogphotos_get_posttype() || is_super_admin() ) {
 		return $edit_url;
 	}
 
-	if ( $post->post_author == get_current_user_id() && ! current_user_can( buddyblog_get_option( 'dashboard_edit_cap' ) ) ) {
-		return buddyblog_get_edit_url( $post_id );
+	if ( $post->post_author == get_current_user_id() && ! current_user_can( buddyblogphotos_get_option( 'dashboard_edit_cap' ) ) ) {
+		return buddyblogphotos_get_edit_url( $post_id );
 	}
 
 	return $edit_url;
 
 }
 
-add_filter( 'get_edit_post_link', 'buddyblog_fix_edit_post_link', 10, 3 );
+add_filter( 'get_edit_post_link', 'buddyblogphotos_fix_edit_post_link', 10, 3 );
 
 /**
  * Cap filtering to show the edit link on posts
@@ -212,9 +212,9 @@ add_filter( 'get_edit_post_link', 'buddyblog_fix_edit_post_link', 10, 3 );
  * Mostly used for checking existing code
  *
  */
-function buddyblog_add_user_can_edit_cap( $allcaps, $cap, $args ) {
+function buddyblogphotos_add_user_can_edit_cap( $allcaps, $cap, $args ) {
 
-	$post_type_obj = get_post_type_object( buddyblog_get_posttype() );
+	$post_type_obj = get_post_type_object( buddyblogphotos_get_posttype() );
 
 	if ( is_null( $post_type_obj ) || $post_type_obj->cap->edit_post != $args[0] ) {
 		return $allcaps;
@@ -222,7 +222,7 @@ function buddyblog_add_user_can_edit_cap( $allcaps, $cap, $args ) {
 
 	$post_id = $args[2];
 
-	if ( buddyblog_user_can_edit( $post_id ) ) {
+	if ( buddyblogphotos_user_can_edit( $post_id ) ) {
 		$allcaps[ $cap[0] ] = true;
 	}
 
@@ -230,16 +230,16 @@ function buddyblog_add_user_can_edit_cap( $allcaps, $cap, $args ) {
 	// give author some permissions
 }
 
-add_filter( 'user_has_cap', 'buddyblog_add_user_can_edit_cap', 0, 3 );
+add_filter( 'user_has_cap', 'buddyblogphotos_add_user_can_edit_cap', 0, 3 );
 
 /**
  * Should we limit user by no. of posts he has made?
  *
  * @return bool
  */
-function buddyblog_limit_no_of_posts() {
+function buddyblogphotos_limit_no_of_posts() {
 
-	return apply_filters( 'buddyblog_limit_no_of_posts', buddyblog_get_option( 'limit_no_of_posts' ) );
+	return apply_filters( 'buddyblogphotos_limit_no_of_posts', buddyblogphotos_get_option( 'limit_no_of_posts' ) );
 }
 
 /**
@@ -247,8 +247,8 @@ function buddyblog_limit_no_of_posts() {
  *
  * @return bool
  */
-function buddyblog_show_posts_on_profile( $post ) {
-	return apply_filters( 'buddyblog_show_posts_on_profile', buddyblog_get_option( 'show_posts_on_profile' ), $post );
+function buddyblogphotos_show_posts_on_profile( $post ) {
+	return apply_filters( 'buddyblogphotos_show_posts_on_profile', buddyblogphotos_get_option( 'show_posts_on_profile' ), $post );
 }
 
 /**
@@ -258,28 +258,27 @@ function buddyblog_show_posts_on_profile( $post ) {
  *
  * @return mixed
  */
-function buddyblog_modify_page_title( $bp_title ) {
+function buddyblogphotos_modify_page_title( $bp_title ) {
 
-	if ( ! bp_is_buddyblog_component() ) {
+	if ( ! bp_is_buddyblogphotos_component() ) {
 		return $bp_title;
 	}
 
-	$post_type_obj = get_post_type_object( buddyblog_get_posttype() );
+	$post_type_obj = get_post_type_object( buddyblogphotos_get_posttype() );
 	$sep           = apply_filters( 'document_title_separator', '-' );
 
-	if ( buddyblog_is_single_post() ) {
-		$post_id           = buddyblog_get_post_id( bp_action_variable( 0 ) );
+	if ( buddyblogphotos_is_single_post() ) {
+		$post_id           = buddyblogphotos_get_post_id( bp_action_variable( 0 ) );
 		$post              = get_post( $post_id );
 		$bp_title['title'] = $post->post_title . ' ' . $sep . ' ' . $bp_title['title'];
 		$bp_title['page']  = '';
-	} elseif ( buddyblog_is_edit_post() ) {
+	} elseif ( buddyblogphotos_is_edit_post() ) {
 		$bp_title['title'] = $post_type_obj->labels->edit_item;
-	} elseif ( buddyblog_is_new_post() ) {
+	} elseif ( buddyblogphotos_is_new_post() ) {
 		$bp_title['title'] = $post_type_obj->labels->new_item;
 	}
 
 	return $bp_title;
 }
 
-add_filter( 'bp_modify_document_title_parts', 'buddyblog_modify_page_title' );
-
+add_filter( 'bp_modify_document_title_parts', 'buddyblogphotos_modify_page_title' );
