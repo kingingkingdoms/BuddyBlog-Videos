de5caec5ffe7035c4a783becb81e5d6795f80fa1
kingingkingdoms <37118190+kingingkingdoms@users.noreply.github.com>
2018-06-01 09:56:29 -0400

    Update buddyblogarticles-filters.php

--- core/buddyblogarticles-filters.php
+++ core/buddyblogarticles-filters.php
@@ -15,15 +15,15 @@
  *
  * @return string
  */
-function buddyblogarticles_fix_comment_form_redirect( $post_id ) {
+function buddyblogvideos_fix_comment_form_redirect( $post_id ) {
 
 	$post = get_post( $post_id );
 
-	if ( ! buddyblogarticles_show_posts_on_profile( $post ) ) {
+	if ( ! buddyblogvideos_show_posts_on_profile( $post ) ) {
 		return $post_id;
 	}
 
-	if ( $post->post_type != buddyblogarticles_get_posttype() ) {
+	if ( $post->post_type != buddyblogvideos_get_posttype() ) {
 		return;
 	}
 
@@ -35,7 +35,7 @@ function buddyblogarticles_fix_comment_form_redirect( $post_id ) {
 	<?php
 }
 
-add_action( 'comment_form', 'buddyblogarticles_fix_comment_form_redirect' );
+add_action( 'comment_form', 'buddyblogvideos_fix_comment_form_redirect' );
 
 /**
  * Get the current editable post id
@@ -46,11 +46,11 @@ add_action( 'comment_form', 'buddyblogarticles_fix_comment_form_redirect' );
  *
  * @return int id of the post to edit
  */
-function buddyblogarticles_get_editable_post_id( $id ) {
+function buddyblogvideos_get_editable_post_id( $id ) {
 
 	$action = bp_current_action();
 
-	if ( bp_is_buddyblogarticles_component() && ( $action == 'edit' ) && bp_action_variable( 0 ) ) {
+	if ( bp_is_buddyblogvideos_component() && ( $action == 'edit' ) && bp_action_variable( 0 ) ) {
 
 		$id = bp_action_variable( 0 );
 	}
@@ -59,7 +59,7 @@ function buddyblogarticles_get_editable_post_id( $id ) {
 	return intval( $id );
 }
 
-add_filter( 'bpsp_editable_post_id', 'buddyblogarticles_get_editable_post_id' );
+add_filter( 'bpsp_editable_post_id', 'buddyblogvideos_get_editable_post_id' );
 
 
 /**
@@ -72,29 +72,29 @@ add_filter( 'bpsp_editable_post_id', 'buddyblogarticles_get_editable_post_id' );
  *
  * @return mixed|string|void
  */
-function buddyblogarticles_filter_posting_message( $message, $post_id, $post_type_obj, $form ) {
+function buddyblogvideos_filter_posting_message( $message, $post_id, $post_type_obj, $form ) {
 
-	if ( $form->get_post_type() != buddyblogarticles_get_posttype() ) {
+	if ( $form->get_post_type() != buddyblogvideos_get_posttype() ) {
 		return $message;
 	}
 
-	if ( ! buddyblogarticles_is_post_published( $post_id ) ) {
+	if ( ! buddyblogvideos_is_post_published( $post_id ) ) {
 		// if the job is not active, let us know that to the user.
-		$message = __( 'Your post was saved.', 'buddyblogarticles' );
+		$message = __( 'Your post was saved.', 'buddyblogvideos' );
 	}
 
 	return $message;
 }
 
-add_filter( 'bsfep_post_success_message', 'buddyblogarticles_filter_posting_message', 10, 4 );
+add_filter( 'bsfep_post_success_message', 'buddyblogvideos_filter_posting_message', 10, 4 );
 
 /**
  * Update the post status based on current user caps
  * If the current user can publish, we will set the status to publish otherwise the status is set to draft by default
  */
 // NOT Needed
-// add_action('bsfep_post_saved','buddyblogarticles_publish_unpublish_on_save');
-function buddyblogarticles_publish_unpublish_on_save( $post_id ) {
+// add_action('bsfep_post_saved','buddyblogvideos_publish_unpublish_on_save');
+function buddyblogvideos_publish_unpublish_on_save( $post_id ) {
 
 	if ( ! empty( $_POST['post_id'] ) ) {
 		return;
@@ -103,12 +103,12 @@ function buddyblogarticles_publish_unpublish_on_save( $post_id ) {
 	// check if it is job post type.
 	$post = get_post( $post_id );
 
-	if ( $post->post_type != buddyblogarticles_get_posttype() ) {
+	if ( $post->post_type != buddyblogvideos_get_posttype() ) {
 		return;
 	}
 
 	// if it is a new post.
-	if ( buddyblogarticles_user_can_publish( get_current_user_id() ) ) {
+	if ( buddyblogvideos_user_can_publish( get_current_user_id() ) ) {
 		//    $active_status=1;
 
 	}
@@ -119,31 +119,31 @@ function buddyblogarticles_publish_unpublish_on_save( $post_id ) {
 /**
  * Fix the post permalink the_permalink to point to user profile/buddyblog/my-posts/ section for custom post type if we are using custom post type
  */
-function buddyblogarticles_fix_permalink( $permalink, $post, $leavename, $sample ) {
+function buddyblogvideos_fix_permalink( $permalink, $post, $leavename, $sample ) {
 
 
-	if ( ! buddyblogarticles_show_posts_on_profile( $post ) ) {
+	if ( ! buddyblogvideos_show_posts_on_profile( $post ) ) {
 		return $permalink;
 	}
 
 
-	$type = buddyblogarticles_get_posttype();
+	$type = buddyblogvideos_get_posttype();
 
 	$user_link = '';
 
 	if ( $post->post_type == $type ) {
 
 		if ( $post->post_status != 'publish' ) {
-			$permalink = buddyblogarticles_get_edit_url( $post->ID );
+			$permalink = buddyblogvideos_get_edit_url( $post->ID );
 		} else {
-			$permalink = buddyblogarticles_get_post_url( $post->ID );
+			$permalink = buddyblogvideos_get_post_url( $post->ID );
 		}
 	}
 
 	return $permalink;
 }
 
-add_filter( 'post_type_link', 'buddyblogarticles_fix_permalink', 10, 4 );
+add_filter( 'post_type_link', 'buddyblogvideos_fix_permalink', 10, 4 );
 
 /**
  * Filter post permalink.
@@ -154,21 +154,21 @@ add_filter( 'post_type_link', 'buddyblogarticles_fix_permalink', 10, 4 );
  *
  * @return string
  */
-function buddyblogarticles_filter_post_permalink( $permalink, $post, $leavename ) {
+function buddyblogvideos_filter_post_permalink( $permalink, $post, $leavename ) {
 
 
-	if ( ! buddyblogarticles_show_posts_on_profile( $post ) ) {
+	if ( ! buddyblogvideos_show_posts_on_profile( $post ) ) {
 		return $permalink;
 	}
 
-	if ( $post->post_type != buddyblogarticles_get_posttype() ) {
+	if ( $post->post_type != buddyblogvideos_get_posttype() ) {
 		return $permalink;
 	}
 
 	if ( $post->post_status != 'publish' ) {
-		$permalink = buddyblogarticles_get_edit_url( $post->ID );
+		$permalink = buddyblogvideos_get_edit_url( $post->ID );
 	} else {
-		$permalink = buddyblogarticles_get_post_url( $post->ID );
+		$permalink = buddyblogvideos_get_post_url( $post->ID );
 	}
 
 	return $permalink;
@@ -187,23 +187,23 @@ add_filter( 'post_link', 'buddybloghotos_filter_post_permalink', 10, 3 );
  *
  * @return string
  */
-function buddyblogarticles_fix_edit_post_link( $edit_url, $post_id, $context ) {
+function buddyblogvideos_fix_edit_post_link( $edit_url, $post_id, $context ) {
 
 	$post = get_post( $post_id );
 
-	if ( $post->post_type != buddyblogarticles_get_posttype() || is_super_admin() ) {
+	if ( $post->post_type != buddyblogvideos_get_posttype() || is_super_admin() ) {
 		return $edit_url;
 	}
 
-	if ( $post->post_author == get_current_user_id() && ! current_user_can( buddyblogarticles_get_option( 'dashboard_edit_cap' ) ) ) {
-		return buddyblogarticles_get_edit_url( $post_id );
+	if ( $post->post_author == get_current_user_id() && ! current_user_can( buddyblogvideos_get_option( 'dashboard_edit_cap' ) ) ) {
+		return buddyblogvideos_get_edit_url( $post_id );
 	}
 
 	return $edit_url;
 
 }
 
-add_filter( 'get_edit_post_link', 'buddyblogarticles_fix_edit_post_link', 10, 3 );
+add_filter( 'get_edit_post_link', 'buddyblogvideos_fix_edit_post_link', 10, 3 );
 
 /**
  * Cap filtering to show the edit link on posts
@@ -212,9 +212,9 @@ add_filter( 'get_edit_post_link', 'buddyblogarticles_fix_edit_post_link', 10, 3 
  * Mostly used for checking existing code
  *
  */
-function buddyblogarticles_add_user_can_edit_cap( $allcaps, $cap, $args ) {
+function buddyblogvideos_add_user_can_edit_cap( $allcaps, $cap, $args ) {
 
-	$post_type_obj = get_post_type_object( buddyblogarticles_get_posttype() );
+	$post_type_obj = get_post_type_object( buddyblogvideos_get_posttype() );
 
 	if ( is_null( $post_type_obj ) || $post_type_obj->cap->edit_post != $args[0] ) {
 		return $allcaps;
@@ -222,7 +222,7 @@ function buddyblogarticles_add_user_can_edit_cap( $allcaps, $cap, $args ) {
 
 	$post_id = $args[2];
 
-	if ( buddyblogarticles_user_can_edit( $post_id ) ) {
+	if ( buddyblogvideos_user_can_edit( $post_id ) ) {
 		$allcaps[ $cap[0] ] = true;
 	}
 
@@ -230,16 +230,16 @@ function buddyblogarticles_add_user_can_edit_cap( $allcaps, $cap, $args ) {
 	// give author some permissions
 }
 
-add_filter( 'user_has_cap', 'buddyblogarticles_add_user_can_edit_cap', 0, 3 );
+add_filter( 'user_has_cap', 'buddyblogvideos_add_user_can_edit_cap', 0, 3 );
 
 /**
  * Should we limit user by no. of posts he has made?
  *
  * @return bool
  */
-function buddyblogarticles_limit_no_of_posts() {
+function buddyblogvideos_limit_no_of_posts() {
 
-	return apply_filters( 'buddyblogarticles_limit_no_of_posts', buddyblogarticles_get_option( 'limit_no_of_posts' ) );
+	return apply_filters( 'buddyblogvideos_limit_no_of_posts', buddyblogvideos_get_option( 'limit_no_of_posts' ) );
 }
 
 /**
@@ -247,8 +247,8 @@ function buddyblogarticles_limit_no_of_posts() {
  *
  * @return bool
  */
-function buddyblogarticles_show_posts_on_profile( $post ) {
-	return apply_filters( 'buddyblogarticles_show_posts_on_profile', buddyblogarticles_get_option( 'show_posts_on_profile' ), $post );
+function buddyblogvideos_show_posts_on_profile( $post ) {
+	return apply_filters( 'buddyblogvideos_show_posts_on_profile', buddyblogvideos_get_option( 'show_posts_on_profile' ), $post );
 }
 
 /**
@@ -258,27 +258,27 @@ function buddyblogarticles_show_posts_on_profile( $post ) {
  *
  * @return mixed
  */
-function buddyblogarticles_modify_page_title( $bp_title ) {
+function buddyblogvideos_modify_page_title( $bp_title ) {
 
-	if ( ! bp_is_buddyblogarticles_component() ) {
+	if ( ! bp_is_buddyblogvideos_component() ) {
 		return $bp_title;
 	}
 
-	$post_type_obj = get_post_type_object( buddyblogarticles_get_posttype() );
+	$post_type_obj = get_post_type_object( buddyblogvideos_get_posttype() );
 	$sep           = apply_filters( 'document_title_separator', '-' );
 
-	if ( buddyblogarticles_is_single_post() ) {
-		$post_id           = buddyblogarticles_get_post_id( bp_action_variable( 0 ) );
+	if ( buddyblogvideos_is_single_post() ) {
+		$post_id           = buddyblogvideos_get_post_id( bp_action_variable( 0 ) );
 		$post              = get_post( $post_id );
 		$bp_title['title'] = $post->post_title . ' ' . $sep . ' ' . $bp_title['title'];
 		$bp_title['page']  = '';
-	} elseif ( buddyblogarticles_is_edit_post() ) {
+	} elseif ( buddyblogvideos_is_edit_post() ) {
 		$bp_title['title'] = $post_type_obj->labels->edit_item;
-	} elseif ( buddyblogarticles_is_new_post() ) {
+	} elseif ( buddyblogvideos_is_new_post() ) {
 		$bp_title['title'] = $post_type_obj->labels->new_item;
 	}
 
 	return $bp_title;
 }
 
-add_filter( 'bp_modify_document_title_parts', 'buddyblogarticles_modify_page_title' );
+add_filter( 'bp_modify_document_title_parts', 'buddyblogvideos_modify_page_title' );
