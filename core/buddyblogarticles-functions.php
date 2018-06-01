035a39d2ab95032b5dde55f50d2ee4ecef7f64fc
kingingkingdoms <37118190+kingingkingdoms@users.noreply.github.com>
2018-06-01 10:03:48 -0400

    Update buddyblogarticles-functions.php

--- core/buddyblogarticles-functions.php
+++ core/buddyblogarticles-functions.php
@@ -16,8 +16,8 @@ if ( ! defined( 'ABSPATH' ) ) {
  *
  * @return bool
  */
-function bp_is_buddyblogarticles_component() {
-	return bp_is_current_component( 'buddyblogarticles' );
+function bp_is_buddyblogvideos_component() {
+	return bp_is_current_component( 'buddyblogvideos' );
 }
 
 /**
@@ -25,14 +25,14 @@ function bp_is_buddyblogarticles_component() {
  *
  * @return string
  */
-function buddyblogarticles_get_posttype() {
-	$post_type = buddyblogarticles_get_option( 'post_type' );
+function buddyblogvideos_get_posttype() {
+	$post_type = buddyblogvideos_get_option( 'post_type' );
 
 	if ( ! $post_type ) {
 		$post_type = 'post';
 	}
 
-	return apply_filters( 'buddyblogarticles_get_post_type', $post_type );
+	return apply_filters( 'buddyblogvideos_get_post_type', $post_type );
 }
 
 /**
@@ -40,9 +40,9 @@ function buddyblogarticles_get_posttype() {
  *
  * @return array
  */
-function buddyblogarticles_get_taxonomies() {
+function buddyblogvideos_get_taxonomies() {
 
-	return apply_filters( 'buddyblogarticles_get_taxonomies', buddyblogarticles_get_option( 'allowed_taxonomies' ) );
+	return apply_filters( 'buddyblogvideos_get_taxonomies', buddyblogvideos_get_option( 'allowed_taxonomies' ) );
 }
 
 /**
@@ -54,7 +54,7 @@ function buddyblogarticles_get_taxonomies() {
  *
  * @todo : may need revisist
  */
-function buddyblogarticles_get_total_posted( $user_id = 0 ) {
+function buddyblogvideos_get_total_posted( $user_id = 0 ) {
 
 	if ( ! $user_id ) {
 		$user_id = bp_displayed_user_id();
@@ -75,7 +75,7 @@ function buddyblogarticles_get_total_posted( $user_id = 0 ) {
  *
  * @return int
  */
-function buddyblogarticles_get_total_published_posts( $user_id = 0 ) {
+function buddyblogvideos_get_total_published_posts( $user_id = 0 ) {
 
 	if ( ! $user_id ) {
 		$user_id = get_current_user_id();
@@ -96,7 +96,7 @@ function buddyblogarticles_get_total_published_posts( $user_id = 0 ) {
  *
  * @return int
  */
-function buddyblogarticles_get_allowed_no_of_posts( $user_id = 0 ) {
+function buddyblogvideos_get_allowed_no_of_posts( $user_id = 0 ) {
 
 	if ( ! $user_id ) {
 		$user_id = bp_displayed_user_id();
@@ -104,7 +104,7 @@ function buddyblogarticles_get_allowed_no_of_posts( $user_id = 0 ) {
 
 	// filter on this hook to change the no. of posts allowed.
 	// by default no. posts allowed.
-	return apply_filters( 'buddyblogarticles_allowed_posts_count', buddyblogarticles_get_option( 'max_allowed_posts' ), $user_id );
+	return apply_filters( 'buddyblogvideos_allowed_posts_count', buddyblogvideos_get_option( 'max_allowed_posts' ), $user_id );
 }
 
 /**
@@ -114,11 +114,11 @@ function buddyblogarticles_get_allowed_no_of_posts( $user_id = 0 ) {
  *
  * @return int
  */
-function buddyblogarticles_get_remaining_posts( $user_id = 0 ) {
+function buddyblogvideos_get_remaining_posts( $user_id = 0 ) {
 
-	$total_allowed = buddyblogarticles_get_allowed_no_of_posts( $user_id );
+	$total_allowed = buddyblogvideos_get_allowed_no_of_posts( $user_id );
 
-	return intval( $total_allowed - buddyblogarticles_get_total_published_posts( $user_id ) );
+	return intval( $total_allowed - buddyblogvideos_get_total_published_posts( $user_id ) );
 }
 
 /**
@@ -126,20 +126,20 @@ function buddyblogarticles_get_remaining_posts( $user_id = 0 ) {
  *
  * @return bool
  */
-function buddyblogarticles_is_single_post() {
+function buddyblogvideos_is_single_post() {
 
 	$action  = bp_current_action();
 	$post_id = 0;
 	// make sure
 	// to check the strategy.
-	if ( buddyblogarticles_use_slug_in_permalink() ) {
+	if ( buddyblogvideos_use_slug_in_permalink() ) {
 		$slug    = bp_action_variable( 0 );
-		$post_id = buddyblogarticles_get_post_id_from_slug( $slug );
+		$post_id = buddyblogvideos_get_post_id_from_slug( $slug );
 	} else {
 		$post_id = intval( bp_action_variable( 0 ) );
 	}
 
-	if ( bp_is_buddyblogarticles_component() && $action == BUDDYBLOGARTICLES_ARCHIVE_SLUG && ! empty( $post_id ) ) {
+	if ( bp_is_buddyblogvideos_component() && $action == BUDDYBLOGARTICLES_ARCHIVE_SLUG && ! empty( $post_id ) ) {
 		return true;
 	}
 
@@ -151,12 +151,12 @@ function buddyblogarticles_is_single_post() {
  *
  * @return bool
  */
-function buddyblogarticles_is_posts_archive() {
+function buddyblogvideos_is_posts_archive() {
 
 	$action  = bp_current_action();
 	$post_id = bp_action_variable( 0 );
 
-	if ( bp_is_buddyblogarticles_component() && $action == BUDDYBLOGARTICLES_ARCHIVE_SLUG && empty( $post_id ) ) {
+	if ( bp_is_buddyblogvideos_component() && $action == BUDDYBLOGARTICLES_ARCHIVE_SLUG && empty( $post_id ) ) {
 		return true;
 	}
 
@@ -168,12 +168,12 @@ function buddyblogarticles_is_posts_archive() {
  *
  * @return bool
  */
-function buddyblogarticles_is_edit_post() {
+function buddyblogvideos_is_edit_post() {
 
 	$action  = bp_current_action();
 	$post_id = bp_action_variable( 0 );
 
-	if ( bp_is_buddyblogarticles_component() && $action == 'edit' && ! empty( $post_id ) ) {
+	if ( bp_is_buddyblogvideos_component() && $action == 'edit' && ! empty( $post_id ) ) {
 		return true;
 	}
 
@@ -185,12 +185,12 @@ function buddyblogarticles_is_edit_post() {
  *
  * @return bool
  */
-function buddyblogarticles_is_new_post() {
+function buddyblogvideos_is_new_post() {
 
 	$action  = bp_current_action();
 	$post_id = bp_action_variable( 0 );
 
-	if ( bp_is_buddyblogarticles_component() && $action == 'edit' && empty( $post_id ) ) {
+	if ( bp_is_buddyblogvideos_component() && $action == 'edit' && empty( $post_id ) ) {
 		return true;
 	}
 
@@ -203,9 +203,9 @@ function buddyblogarticles_is_new_post() {
  *
  * @return bool
  */
-function buddyblogarticles_user_has_posted() {
+function buddyblogvideos_user_has_posted() {
 
-	$total_posts = buddyblogarticles_get_total_posted();
+	$total_posts = buddyblogvideos_get_total_posted();
 
 	return (bool) $total_posts;
 }
@@ -217,13 +217,13 @@ function buddyblogarticles_user_has_posted() {
  *
  * @return string
  */
-function buddyblogarticles_get_home_url( $user_id = false ) {
+function buddyblogvideos_get_home_url( $user_id = false ) {
 
 	if ( ! $user_id ) {
 		$user_id = bp_displayed_user_id();
 	}
 
-	$url = bp_core_get_user_domain( $user_id ) . buddypress()->buddyblogarticles->slug . '/';
+	$url = bp_core_get_user_domain( $user_id ) . buddypress()->buddyblogvideos->slug . '/';
 
 	return $url;
 }
@@ -235,7 +235,7 @@ function buddyblogarticles_get_home_url( $user_id = false ) {
  *
  * @return string
  */
-function buddyblogarticles_get_post_publish_unpublish_url( $post_id = 0 ) {
+function buddyblogvideos_get_post_publish_unpublish_url( $post_id = 0 ) {
 
 	if ( ! $post_id ) {
 		return '';
@@ -244,11 +244,11 @@ function buddyblogarticles_get_post_publish_unpublish_url( $post_id = 0 ) {
 	$post = get_post( $post_id );
 	$url  = '';
 
-	if ( buddyblogarticles_user_can_publish( get_current_user_id(), $post_id ) ) {
+	if ( buddyblogvideos_user_can_publish( get_current_user_id(), $post_id ) ) {
 		// check if post is published.
-		$url = buddyblogarticles_get_home_url( $post->post_author );
+		$url = buddyblogvideos_get_home_url( $post->post_author );
 
-		if ( buddyblogarticles_is_post_published( $post_id ) ) {
+		if ( buddyblogvideos_is_post_published( $post_id ) ) {
 			$url = $url . 'unpublish/' . $post_id . '/';
 		} else {
 			$url = $url . 'publish/' . $post_id . '/';
@@ -268,31 +268,31 @@ function buddyblogarticles_get_post_publish_unpublish_url( $post_id = 0 ) {
  *
  * @return string link
  */
-function buddyblogarticles_get_post_publish_unpublish_link( $post_id = 0, $label_ac = '', $label_de = '' ) {
+function buddyblogvideos_get_post_publish_unpublish_link( $post_id = 0, $label_ac = '', $label_de = '' ) {
 
 	if ( ! $post_id ) {
 		return '';
 	}
 
-	$is_published = buddyblogarticles_is_post_published( $post_id );
+	$is_published = buddyblogvideos_is_post_published( $post_id );
 
-	if ( $is_published && ! buddyblogarticles_user_can_unpublish( get_current_user_id(), $post_id ) ) {
+	if ( $is_published && ! buddyblogvideos_user_can_unpublish( get_current_user_id(), $post_id ) ) {
 		return '';
-	} elseif ( ! $is_published && ! buddyblogarticles_user_can_publish( get_current_user_id(), $post_id ) ) {
+	} elseif ( ! $is_published && ! buddyblogvideos_user_can_publish( get_current_user_id(), $post_id ) ) {
 		return '';
 	}
 
 	$post = get_post( $post_id );
 
 	$url = '';
-	$url = buddyblogarticles_get_post_publish_unpublish_url( $post_id );
+	$url = buddyblogvideos_get_post_publish_unpublish_url( $post_id );
 
 	if ( empty( $label_ac ) ) {
-		$label_ac = __( 'Publish', 'buddyblogarticles' );
+		$label_ac = __( 'Publish', 'buddyblogvideos' );
 	}
 
 	if ( empty( $label_de ) ) {
-		$label_de = __( 'Unpublish', 'buddyblogarticles' );
+		$label_de = __( 'Unpublish', 'buddyblogvideos' );
 	}
 
 	if ( $is_published ) {
@@ -311,7 +311,7 @@ function buddyblogarticles_get_post_publish_unpublish_link( $post_id = 0, $label
  *
  * @return bool
  */
-function buddyblogarticles_is_post_published( $post_id ) {
+function buddyblogvideos_is_post_published( $post_id ) {
 	return get_post_field( 'post_status', $post_id ) == 'publish';
 }
 
@@ -320,8 +320,8 @@ function buddyblogarticles_is_post_published( $post_id ) {
  *
  * @return bool
  */
-function buddyblogarticles_use_slug_in_permalink() {
-	return apply_filters( 'buddyblogarticles_use_slug_in_permalink', false ); // Whether to use id or slug in permalink.
+function buddyblogvideos_use_slug_in_permalink() {
+	return apply_filters( 'buddyblogvideos_use_slug_in_permalink', false ); // Whether to use id or slug in permalink.
 }
 
 /**
@@ -331,13 +331,13 @@ function buddyblogarticles_use_slug_in_permalink() {
  *
  * @return int ID of Post
  */
-function buddyblogarticles_get_post_id_from_slug( $slug ) {
+function buddyblogvideos_get_post_id_from_slug( $slug ) {
 
 	if ( ! $slug ) {
 		return 0;
 	}
 
-	$post = get_page_by_path( $slug, false, buddyblogarticles_get_posttype() );
+	$post = get_page_by_path( $slug, false, buddyblogvideos_get_posttype() );
 
 	if ( $post ) {
 		return $post->ID;
@@ -354,14 +354,14 @@ function buddyblogarticles_get_post_id_from_slug( $slug ) {
  *
  * @return int ID of Post
  */
-function buddyblogarticles_get_post_id( $slug_or_id ) {
+function buddyblogvideos_get_post_id( $slug_or_id ) {
 
 	if ( is_numeric( $slug_or_id ) ) {
 		return absint( $slug_or_id );
 	}
 
 	// otherwise.
-	return buddyblogarticles_get_post_id_from_slug( $slug_or_id );
+	return buddyblogvideos_get_post_id_from_slug( $slug_or_id );
 }
 
 /**
@@ -371,9 +371,9 @@ function buddyblogarticles_get_post_id( $slug_or_id ) {
  *
  * @return mixed
  */
-function buddyblogarticles_get_option( $option_name ) {
+function buddyblogvideos_get_option( $option_name ) {
 
-	$settings = buddyblogarticles_get_settings();
+	$settings = buddyblogvideos_get_settings();
 
 	if ( isset( $settings[ $option_name ] ) ) {
 		return $settings[ $option_name ];
@@ -389,8 +389,8 @@ function buddyblogarticles_get_option( $option_name ) {
  *
  * @return boolean
  */
-function buddyblogarticles_is_buddyblogarticles_post( $post_id ) {
-	return get_post_meta( $post_id, '_is_buddyblogarticles_post', true );
+function buddyblogvideos_is_buddyblogvideos_post( $post_id ) {
+	return get_post_meta( $post_id, '_is_buddyblogvideos_post', true );
 }
 
 /**
@@ -398,11 +398,11 @@ function buddyblogarticles_is_buddyblogarticles_post( $post_id ) {
  *
  * @return array
  */
-function buddyblogarticles_get_settings() {
+function buddyblogvideos_get_settings() {
 
 	$default = array(
-		//'root_slug'		=> 'buddyblogarticles',
-		'post_type'             => 'article',
+		//'root_slug'		=> 'buddyblogvideos',
+		'post_type'             => 'video',
 		'post_status'           => 'publish',
 		'comment_status'        => 'open',
 		'show_comment_option'   => 1,
@@ -427,5 +427,5 @@ function buddyblogarticles_get_settings() {
 		'post_update_redirect'  => 'archive',
 	);
 
-	return bp_get_option( 'buddyblogarticles-settings', $default );
+	return bp_get_option( 'buddyblogvideos-settings', $default );
 }
